2e50bdc0d871e25f7f88373ba32f76d3f33d9853
Sergey Herklotz <58405104+UnDevil665@users.noreply.github.com>
2021-05-09 10:55:43 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-sudo apt install python3-pip python3-dev python3-venv libpq-dev postgresql postgresql-contrib nginx curl
+sudo apt install python3-pip python3-dev python3-venv libpq-dev mysql-client mysql-server nginx curl
