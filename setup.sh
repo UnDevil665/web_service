bad9883f61b2bf22b15ff04efd269174246ac568
Sergey Herklotz@ <gerklots.s.s@yandex.ru>
2021-05-09 21:56:14 +0300

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-sudo apt install python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx curl libmysqlclient-dev
+sudo apt install python3-pip python3-dev libpq-dev mysql-client mysql-server nginx curl libmysqlclient-dev
